20027437ff48f28c30a3853377afed9f17434fe6
Krunalgiri Goswami <krunalgiricg2004@gmail.com>
2024-07-25 21:28:15 +0530

    updated note-keeper

--- React/note-keeper/src/App.jsx
+++ React/note-keeper/src/App.jsx
@@ -1,13 +1,20 @@
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import Note from "./components/Note.jsx";
+import notes from "./components/notes.js";
 import "./index.css";
 
 function App() {
   return (
     <>
       <Header />
-      <Note />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.id}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </>
   );
---
+++ React/note-keeper/src/components/notes.js
@@ -0,0 +1,32 @@
+const notes = [
+  {
+    id: 1,
+    title: "Delagation",
+    content:
+      "Q. How Many programmers does it take to change a light bulb?A. None _It's a hardware problem",
+  },
+  {
+    id: 2,
+    title: "Loops",
+    content:
+      "How to keep a programmer in the shower forever. Show him the shampoo bottle instruction Lather. Rinse,repeat.",
+  },
+  {
+    id: 3,
+    title: "Arrays",
+    content:
+      "Why did the programmer quit his job? A. Beacause he didn't get arrays.",
+  },
+  {
+    id: 4,
+    title: "Hardware Vs. Software",
+    content:
+      "What's the difference between hardware and software? you can hit your hardware with a harmer, but you can only curse at your software.",
+  },
+  {
+    id: 5,
+    title: "Big Ideas",
+    content: "Eat more sushi",
+  },
+];
+export default notes;
